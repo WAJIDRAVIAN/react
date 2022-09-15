a952e87485fd61d242131f3b96da9ac4a3274c33
wajidravian <wajidh1997@gmail.com>
2022-09-15 15:26:03 +0500

    seasonConfiguration

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,6 +1,18 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 
+
+const seasonConfig={
+    Summer:{
+        text: 'let\'s hit the beach',
+        iconName: 'sun icon'
+    },
+    Winter:{
+        text: 'It\'s already chilling',
+        iconName: 'snowFlake icon'
+    }
+}
+
 const getSeason =(lat, month)=>{
     if (month>2 && month<9) {
        return lat > 0 ? "Summer" : "Winter";
@@ -11,14 +23,13 @@ const getSeason =(lat, month)=>{
 }
 const SeasonDisplay =props=>{
     const season = getSeason(props.lat, new Date().getMonth());
-    
-    const icon = season==='Winter'? 'snowflake icon' : 'sun icon';
+    const {text, iconName} = seasonConfig[season];
     return  (
         <div>
-            <i className={`${icon}`}/>
+            <i className={`${iconName}`}/>
             <br/>
-             <h1>{season}</h1>
-             <i className={`${icon}`}/>
+             <h1 >{text}</h1>
+             <i className={`${iconName}`}/>
         </div>
     );
 };
