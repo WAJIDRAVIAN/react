9c5c1ba2131852c9ff983ef706d2d6800eae34b0
wajidravian <wajidh1997@gmail.com>
2022-09-15 13:54:34 +0500

    Season Determined on the basis of Month and latitude

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 
-
+const getSeason =(lat, month)=>{
+    if (month>2 && month<9) {
+       return lat > 0 ? "Summer" : "Winter";
+    }
+}
 const SeasonDisplay =props=>{
+    const season = getSeason(props.lat, new Date().getMonth());
 
     return  (
         <div>SeasonDisplay
             <br/>
-            Latitude:{props.lat}
+            Season: {season}
         </div>
     );
 };
