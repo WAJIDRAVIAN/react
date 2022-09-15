d5b3ba1bfacdadd1cc0669e14679d3127c71fcde
wajidravian <wajidh1997@gmail.com>
2022-09-15 15:12:00 +0500

    seasonal icons attached dynamiclly using semantic-ui

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -5,14 +5,20 @@ const getSeason =(lat, month)=>{
     if (month>2 && month<9) {
        return lat > 0 ? "Summer" : "Winter";
     }
+    else{
+        return lat > 0 ? "Winter" : "Summer";
+    }
 }
 const SeasonDisplay =props=>{
     const season = getSeason(props.lat, new Date().getMonth());
-
+    
+    const icon = season==='Winter'? 'snowflake icon' : 'sun icon';
     return  (
-        <div>SeasonDisplay
+        <div>
+            <i className={`${icon}`}/>
             <br/>
-            Season: {season}
+             <h1>{season}</h1>
+             <i className={`${icon}`}/>
         </div>
     );
 };
