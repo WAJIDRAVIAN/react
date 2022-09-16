351851ffc04487236f4c1c3b0c84ca8ba78e0f96
wajidravian <wajidh1997@gmail.com>
2022-09-16 18:05:30 +0500

    Rendering Lists

---
+++ Udemy Course Work/pics/src/components/ImageList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ImageList = ()=>{
+return (
+    <div>
+        <ul>
+            Image List
+            <li></li>
+        </ul>
+    </div>
+    );
+};
+
+export default ImageList;
