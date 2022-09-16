11e38f8978fa8f92902dde90615a41592624fa04
wajidravian <wajidh1997@gmail.com>
2022-09-16 17:57:57 +0500

    created custom clients

--- Udemy Course Work/pics/src/components/App.js
+++ Udemy Course Work/pics/src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+import Unsplash from '../api/Unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
     state = {images: []};
     onSearchSubmit= async(term)=>{
         // console.log(term);
-        const response = await axios.get('https://api.unsplash.com/search/photos',{
-        params:{query:term},  
-        headers:{
-            Authorization: 'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296'
-            }
+        const response = await Unsplash.get('/search/photos',{
+        params:{query:term} 
         });
 
         // console.log(response.data.results);
