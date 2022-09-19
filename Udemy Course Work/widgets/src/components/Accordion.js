7c0ecf3f49991ff3c9162d6b9f10f8cb36540661
wajidravian <wajidh1997@gmail.com>
2022-09-19 14:33:06 +0500

    Helper Functions in Function Components

--- Udemy Course Work/widgets/src/components/Accordion.js
+++ Udemy Course Work/widgets/src/components/Accordion.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 
-
 const Accordion=({items})=>{
+    const onTitleClick=(index)=>{
+        console.log('Title clicked:', index)
+    }
+
     const renderedItems = items.map((item,  index)=>{
         return(
             <React.Fragment key={item.title}>
-                    <div className="title active" onClick={()=>console.log('Item clicked:',index)}>
+                    <div className="title active" onClick={()=>onTitleClick(index)}>
                         <i className="dropdown icon"></i>
                         {item.title}
                     </div>
