835f614d11c5e8a6f6e05d8848b81fbfd879fc45
wajidravian <wajidh1997@gmail.com>
2022-09-19 16:32:55 +0500

    dynamic active and Expanding the Accordion

--- Udemy Course Work/widgets/src/components/Accordion.js
+++ Udemy Course Work/widgets/src/components/Accordion.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 
-
 const Accordion=({items})=>{
     const [activeIndex, setActiveIndex] = useState(null);
     const onTitleClick=(index)=>{
@@ -9,16 +8,17 @@ const Accordion=({items})=>{
     }
 
     const renderedItems = items.map((item,  index)=>{
+        const active = index===activeIndex ? 'active' : '';
+
         return(
             <React.Fragment key={item.title}>
-                    <div className="title active" onClick={()=>onTitleClick(index)}>
+                    <div className={`title ${active}`} onClick={()=>onTitleClick(index)}>
                         <i className="dropdown icon"></i>
                         {item.title}
                     </div>
-                    <div className="content active">
+                    <div className={`content ${active}`}>
                         <p> {item.content} </p>
                     </div>
-                
             </React.Fragment>
         );
     });
