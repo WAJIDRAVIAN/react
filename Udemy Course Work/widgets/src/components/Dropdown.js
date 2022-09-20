bfd7eb30319756b3791e6f89e65dc4f3c2b6c0eb
wajidravian <wajidh1997@gmail.com>
2022-09-20 10:23:59 +0500

    dorpdown menu and styling using semantic-ui

--- Udemy Course Work/widgets/src/components/Dropdown.js
+++ Udemy Course Work/widgets/src/components/Dropdown.js
@@ -1,10 +1,27 @@
 import React from "react";
 
 const Dropdown = ({options})=>{
+    const renderedOptions = options.map((option)=>{
+        return(
+            <div key={option.value}>
+                {option.label}
+            </div>
+        );
+    })
 return (
-<div>
-    <h1> Dropdown</h1>
-</div>
+    <div className="ui form">
+        <div className="field">
+            <label className="ui label">Select a color</label>
+            <div className="ui selection dropdown visible active">
+                <i className="ui dropdown icon"></i>
+                <div className="text">Select option</div>
+                <div className="ui menu visible transition">
+                    {renderedOptions}
+                </div>
+            </div>
+        </div>
+        
+    </div>
 );
 }
 
