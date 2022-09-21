dbc7e5201c1ecbd24be508e41c47598ff49e18e6
wajidravian <wajidh1997@gmail.com>
2022-09-21 17:06:22 +0500

    Configuring Connect with MapStateToProps

--- Udemy Course Work/songs/src/components/SongList.js
+++ Udemy Course Work/songs/src/components/SongList.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import {connect} from 'react-redux';
 
 class SongList extends Component{
+    
     render(){
+        
         return(
             <div>
                 Songs List
             </div>
         );
     }
+};
+
+const mapStateToProps =(state)=>{
+    console.log('Wonderful',  state);
+    return {songs:state.songs};
 }
 
-export default connect ()(SongList);
+export default connect (mapStateToProps)(SongList);
