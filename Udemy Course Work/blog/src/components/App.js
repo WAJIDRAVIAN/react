cc4a59ae3b8851760ed19f4cb11d3c0a2966775e
wajidravian <wajidh1997@gmail.com>
2022-09-22 11:06:46 +0500

    Initial App Setup

---
+++ Udemy Course Work/blog/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () =>{
+    return(
+        <div>
+            <header>
+                <h1>App</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
