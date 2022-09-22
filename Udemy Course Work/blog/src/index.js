cd0aec4ecc91ca8c79b06d3b3ab4ab02d864b6d7
wajidravian <wajidh1997@gmail.com>
2022-09-22 11:13:06 +0500

    Tricking Redux with Dummy Reducers

--- Udemy Course Work/blog/src/index.js
+++ Udemy Course Work/blog/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
+import reducers from "./reducers";
 import App from "./components/App";
 
 ReactDOM.createRoot(document.querySelector('#root')).render(
