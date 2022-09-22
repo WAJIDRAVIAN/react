563b9fcfb7fd46b6cd0db2108137e30633ca37d9
wajidravian <wajidh1997@gmail.com>
2022-09-22 11:22:57 +0500

    A Touch More Setup

--- Udemy Course Work/blog/src/components/App.js
+++ Udemy Course Work/blog/src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import PostList from "./PostList";
 const App = () =>{
     return(
         <div>
-            <header>
-                <h1>App</h1>
+            <header className="header">
+                <h1>Blog</h1>
             </header>
+            <div className="ui container">
+                <PostList/>
+            </div>
         </div>
     );
 }
