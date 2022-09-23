61711e44f7cf1f142b1246387b715c90663ada58
wajidravian <wajidh1997@gmail.com>
2022-09-23 15:42:38 +0500

    Introducing React Router

--- Udemy Course Work/streams/client/src/components/App.js
+++ Udemy Course Work/streams/client/src/components/App.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () =>{
+    return (
+        <div>
+            <h1>PageOne</h1>
+        </div>
+    );
+}
+
+const PageTwo = () =>{
+    return (
+        <div>
+            <h1>PageTwo</h1>
+        </div>
+    );
+}
+
 
 const App = () =>{
     return (
         <div>
             <h1>App</h1>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={PageOne}/>
+                    <Route path="/pagetwo" component={PageTwo}/>
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
