ec65e06d6ac67e9b3b18abefa269aed5561a0c94
wajidravian <wajidh1997@gmail.com>
2022-09-23 17:25:54 +0500

    Navigating with React Router

--- Udemy Course Work/streams/client/src/components/App.js
+++ Udemy Course Work/streams/client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () =>{
     return (
         <div>
             <h1>PageOne</h1>
-            <a href='/pagetwo'>Navigate to Page Two</a>
+            <Link to='/pagetwo'>Navigate to Page Two</Link>
         </div>
     );
 }
@@ -14,7 +14,7 @@ const PageTwo = () =>{
     return (
         <div>
             <h1>PageTwo</h1>
-            <a href='/'>Navigate to Page One</a>
+            <Link to='/'>Navigate to Page One</Link>
         </div>
     );
 }
